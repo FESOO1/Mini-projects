05982d4738fc8afb58cb803e9f2b3a36e206e867
FES001 <firdavs7794948@gmail.com>
2024-01-23 22:38:20 +0500

    s

--- toggle-button/script.js
+++ toggle-button/script.js
@@ -1,5 +1,6 @@
 let body = document.querySelector('body');
 let switchBtn = document.querySelector('.switch');
+let container = document.querySelector('.toggle-container');
 
 document.querySelector('.switch').addEventListener('click', () => {
     body.classList.toggle('switcher');
@@ -8,4 +9,8 @@ document.querySelector('.switch').addEventListener('click', () => {
 
 document.querySelector('.switch').addEventListener('click', () => {
     switchBtn.classList.toggle('switchbtn');
+})
+
+document.querySelector('.switch').addEventListener('click', () => {
+    container.classList.toggle('container');
 })
